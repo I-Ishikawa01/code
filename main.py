0ea97d40ac6672a84ed7ace47c95d5b9f24c7f65
i.ishikawa <snoopyrollcake@gmail.com>
2025-08-02 13:40:44 +0900

    main.pyとpage1.pyを変更し，st.session_stateがページを超えて保存されることを確認

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import streamlit as st
 
-st.title("名前記憶アプリ")
+# 1ターム目
+# st.title("名前記憶アプリ")
+# 2ターム目
+st.title("ユーザー情報入力")
 
-if "name" not in st.session_state:
-    st.session_state.name = ""
+# 1ターム目
+# if "name" not in st.session_state:
+#     st.session_state.name = ""
+# 2ターム目
+if "user_name" not in st.session_state:
+    st.session_state.user_name = ""
 
 name = st.text_input("あなたの名前を入力してください")
-if st.button("名前を記憶"):
-    st.session_state.name = name
+if st.button("名前を保存"):
+    # st.session_state.name = name
+    st.session_state.user_name = name
+    st.success("名前を保存しました！")
 
-st.write(f"記憶している名前：{st.session_state.name}")
+# st.write(f"記憶している名前：{st.session_state.name}")
+st.write(f"現在保存されている名前：{st.session_state.user_name}")
--- pages/page1.py
+++ pages/page1.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.write("おはよう")
+# 1ターム目
+# st.write("おはよう")
+
+# 2ターム目
+st.title("ユーザー情報表示ページ")
+
+# session_stateからデータを取得
+if  "user_name" in st.session_state and st.session_state.user_name:
+    st.success(f"こんにちは，{st.session_state.user_name}")
+    st.write("メインパージで入力された名前が正しく表示されています．")
+
+    # 追加の表示
+    st.balloons()    # 祝福のアニメーション
+
+else:
+    st.error("ユーザ名が設定されていません")
+    st.write("メインページで名前を入力してください")
